fdabdc74fcbc35aa3e2c4167495c8cedbaae53cc
Vyctoria <vyctoriak@gmail.com>
2020-07-10 17:31:01 -0300

    mudando hooks do useEffect pra tirar loop de chamadas, fazendo busca pelo campo de input, css da home e detalhes feitos, melhorando código da home

--- src/Pages/Details/details.js
+++ src/Pages/Details/details.js
@@ -3,86 +3,147 @@ import { useParams, Link } from 'react-router-dom';
 import '../../assets/styles/details/details.css';
 import api from '../../requests/api';
 
+const key = {
+  public_key: "5aac5bc626f58d061575c2cb8f0e446e",
+  timestamp: "1594395237695",
+  hash: "d065183a15d9685ba03ca919ec79c54e"
+}
+
 function Details() {
   let { id } = useParams();
 
-  const [detailsCharacters, setDetailsCharacters] = useState(null);
+  const [detailsCharacters, setDetailsCharacters] = useState([]);
+  const [comics, setComics] = useState([]);
+  
+  useEffect( () => {
+    console.log(detailsCharacters)
+    console.log(comics)
+  }, [detailsCharacters, comics])
+  
+  useEffect( () => {
+    getCharactersById(id)
+    getComicsById(id);
+  }, [id])
 
   // Minha key
   // const public_key = '5aac5bc626f58d061575c2cb8f0e446e';
+  
+  async function getCharactersById (param) {
+    try {
+      const response = await api.get(`characters/${param}?&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`)
+      const { results } = response.data.data;
+      setDetailsCharacters(results);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function getComicsById (param) {
+    try {
+      // const response = await api.get(api.get(`/v1/public/characters/${param}/comics?format=comic&formatType=comic&orderBy=onsaleDate&limit=10&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`))
+      
+      const response = await api.get(`characters/${param}/comics?format=comic&formatType=comic&orderBy=onsaleDate&limit=10&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`)
+      console.log( "response:",response);
+      const { results } = response.data.data;
+      setComics(results);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
 
   // key de outra conta
   // const public_key = 'a426809c0b8fe784902b489148a23319';
 
+  // key da mh conta hotmail
+  // const key = {
+  //   public_key: "86cfff8f9319d4fae2be20a7cd1b2ee1",
+  //   timestamp: "1594345144761",
+  //   hash: "52206ae592ca805795a9fa2340905fcf",
+  // }
+
+
   // useEffect( () => {
-  //   api.get(`/v1/public/characters/${id}?&apikey=${public_key}`)
+  //   api.get(`/v1/public/characters/${id}?&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`)
   //     .then(res => setDetailsCharacters(res.data.data.results))
-  // })
+  // }, [id, key.public_key, key.timestamp, key.hash])
+  
+  // useEffect( () => {
+  //   api.get(`/v1/public/characters/${id}/comics?format=comic&formatType=comic&orderBy=onsaleDate&limit=10&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`)
+  //   .then( res => setComics(res.data.data.results))
+  // },[id, key.public_key, key.timestamp, key.hash])
 
-  const mock_info = [
-    {
-      id: 1,
-      name: "Homem-aranha",
-      image: "https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
-      description: "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
+  // https://gateway.marvel.com:443/v1/public/characters/1009664/comics?format=comic&formatType=comic&orderBy=onsaleDate&limit=10&apikey=5aac5bc626f58d061575c2cb8f0e446e
 
-    }
-  ]
+  // const mock_info = [
+  //   {
+  //     id: 1,
+  //     name: "Homem-aranha",
+  //     image: "https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
+  //     description: "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
+
+  //   }
+  // ]
 
 
   return(
     <div className="container-details">
-      {/* {detailsCharacters && detailsCharacters.map( item => (
-        <>
-        <div key={item.id} className="info-characters" 
-        // style={{backgroundImage: `url(${item.thumbnail.path}.${item.thumbnail.extension})`}}
-        >
-          <h1>{item.name}</h1>
-          <p>{item.description}</p>
-        </div>
 
-        <div>
-          teste 
-        </div>
-        </>
-      ))} */}
+      {detailsCharacters && detailsCharacters.map( item => (
+        <div key={item.id}>
+          <div  className="info-characters">
 
-      {mock_info && mock_info.map( item => (
-        <>
-        <div key={item.id} className="info-characters" 
-        // style={ {backgroundImage: `url(${item.image})`} }
-        // style={{backgroundImage: `url(${item.thumbnail.path}.${item.thumbnail.extension})`}}
-        >
+            <div className="photo">
+                <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt={item.name}/>
+            </div>
 
-        
+            <div className="informations">
 
-          <div className="photo">
-              <img src={item.image} alt={item.image}/>
+            <Link to="/" className="page-back">
+                <i className="fas fa-times"></i>
+            </Link>
+              
+              <h1>
+                {item.name}
+              </h1>
+              <p>
+                {item.description}
+              </p>
+            </div>
+
           </div>
 
-          <div className="informations">
-
-          <Link to="/" className="page-back">
-              <i class="fas fa-chevron-left"></i>
-          </Link>
-            
-            <h1>
-              {item.name}
-            </h1>
-            <p>
-              {item.description}
-            </p>
-          </div>
-
+        <div className="divisor">
+          <h1>As últimas comics onde você encontra o(a) personagem <strong> {item.name} </strong></h1>
         </div>
 
-        <div>
-          teste 
+        <div className="comics-container">
+          {comics && comics.map( item => (
+            <div key={item.id} className="comics-item">
+
+              <div className="comic-poster">
+                <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt={item.title}/>
+              </div>
+
+              <div className="comic-informations">
+                <h2>
+                  {item.title}
+                </h2>
+                <p>
+                  {item.description}
+                </p>
+
+              </div>
+            </div>
+          )) }
         </div>
 
-        
-        </>
+        </div>
       ))}
+
+      
 
 
     </div>
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -2,16 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../requests/api';
 import '../../assets/styles/home/home.css';
+import debounce from 'lodash/debounce';
 
 function Home() {
 
-  const [characters, setCharacters] = useState(null);
+  const [characters, setCharacters] = useState([]);
 
   // MINHAS KEYS
   // const public_key = '5aac5bc626f58d061575c2cb8f0e446e';
   // const timestamp = '1594251731556';
   // const hash = 'bf688566c0f2c1f5e84773e0dd3141f6'
   // const limit = 8;
+
+  const key = {
+    public_key: "5aac5bc626f58d061575c2cb8f0e446e",
+    timestamp: "1594395237695",
+    hash: "d065183a15d9685ba03ca919ec79c54e",
+    limit: "20"
+  }
 
   // key de outra conta
   // const key = {
@@ -21,25 +29,78 @@ function Home() {
   //   limit: "4"
   // }
 
+  // key da mh conta hotmail
+  // const key = {
+  //   public_key: "86cfff8f9319d4fae2be20a7cd1b2ee1",
+  //   timestamp: "1594345144761",
+  //   hash: "52206ae592ca805795a9fa2340905fcf",
+  //   limit: 2
+  // }
+
   // https://gateway.marvel.com:443/v1/public/characters/1011334?apikey=a426809c0b8fe784902b489148a23319&ts=1594247367214&hash=84e25fe88a9ef7c89ea1f9ccd0680ac1
 
   // https://gateway.marvel.com/v1/public/characters?offset=0&limit=1&apikey=5aac5bc626f58d061575c2cb8f0e446e&ts=1594251731556&hash=bf688566c0f2c1f5e84773e0dd3141f6
 
-  const mock_info = [
-    {
-      id: 1,
-      name: "Homem-aranha",
-      image: "https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
-      description: "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
+  // const mock_info = [
+  //   {
+  //     id: 1,
+  //     name: "Homem-aranha",
+  //     image: "https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
+  //     description: "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles.",
 
+  //   }
+  // ]
+
+  useEffect( () => {
+    console.log(characters)
+  }, [characters])
+
+  
+  useEffect( () => {
+    getCharacters()
+  }, [])
+
+  async function getCharacters (param) {
+    console.log("event:", param)
+    let route = ''
+    if(param) {
+      route = `characters?name=${param}&orderBy=name&offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
+    } else {
+      route = `characters?orderBy=name&offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
     }
-  ]
+
+    try {
+      // const response = await api.get(`/v1/public/characters?orderBy=name&offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`);
+      console.log(route)
+      const response = await api.get(route)
+      console.log('response:', response)
+      const { data } = response.data;
+      setCharacters(data.results);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const search = (event) => {
+    event.persist();
+    let debouncedFn;
+    if (!debouncedFn) {
+      debouncedFn = debounce(() => {
+         let searchString = event.target.value;
+         if (searchString.length > 3) {
+           getCharacters(searchString);
+         }
+      }, 300);
+    }
+    
+    debouncedFn();
+  }
 
 
-  // useEffect(() => {
-  //   api.get(`/v1/public/characters?offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`)
-  //     .then(res => setCharacters(res.data.data.results))
-  // }, [key.hash, key.limit, key.public_key, key.timestamp])
+  // function search(e) {
+  //   debounce(getCharacters(e.target.value), 500, { 'maxWait': 1000 })
+  // }
+
 
   return (
     <>
@@ -50,31 +111,20 @@ function Home() {
       <label htmlFor="search">
         <img src="https://cdn.ome.lt/Lk57ZJ11LTeHJbYUrkOKLE3LNFA=/570x0/smart/uploads/conteudo/fotos/marvel-2_QIq20gY.jpg" alt="Logo Marvel"/>
       </label>
-      <input type="search" name="search" id="search"/>
+      <input type="search" name="search" id="search" 
+      // onChange={(event) => debounce(getCharacters(event), 500)} 
+      onChange={search}
+      />
     </nav>
     
     <div className="container-characters">
-      {/* {characters && characters.map( item => (
-        <Link key={item.id} to={`/characters/${item.id}`}>
-          <div className="card">
-            <div className="card-poster">
-              <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt={item.name}/>
-            </div>
-            <div className="card-info">
-              <h3>{item.name}</h3>
-              <i className="fa fa-heart" aria-hidden="true"></i>
-            </div>
-          </div>
-        </Link>
-      ))} */}
 
-      {mock_info && mock_info.map( item => (
+      {characters && characters.map( item => (
         <Link key={item.id} to={`/characters/${item.id}`}>
           <div className="card">
             <div className="card-poster">
               <img 
-              src={item.image}
-              // src={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
+              src={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
               alt={item.name}/>
             </div>
             <div className="card-info">
