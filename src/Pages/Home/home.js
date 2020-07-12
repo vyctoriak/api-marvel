d5f73d676877d7aa3874b945e38fe9bf72b392de
Vyctoria <vyctoriak@gmail.com>
2020-07-11 23:55:55 -0300

    favoritando e desafavoritando até 5, botao carregar mais personagens de 100 em 100 após os primeiros 20 mostrados inicialmente no loadMore, melhorando lógica de busca no input, removendo limit do obg key, colocando loading, key id no map, lógica de habilitado ou desabilitado no botão carregar mais

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -8,36 +8,66 @@ function Home() {
 
   const [characters, setCharacters] = useState([]);
   const [arrayOfLike, setArrayOfLike] = useState([]);
+  const [totalOfCharacters, setTotalOfCharacters] = useState();
+  const [limitOfCharacters, setLimitOfCharacters] = useState(20);
+  const [searchString, setSearchString] = useState();
+  const [loading, setLoading] = useState(false);
+  const [offsetOfCharacters, setOffsetOfCharacters] = useState(0);
 
   const key = {
     public_key: "5aac5bc626f58d061575c2cb8f0e446e",
     timestamp: "1594395237695",
-    hash: "d065183a15d9685ba03ca919ec79c54e",
-    limit: "20"
+    hash: "d065183a15d9685ba03ca919ec79c54e"
   };
 
   useEffect( () => {
-    getCharacters();
+    getCharacters('');
     setArrayOfLike(JSON.parse(localStorage.getItem('like')) || {});
   }, [])
 
-  async function getCharacters (param) {
+  async function getCharacters (param, limit, offset) {
+    setLoading(true);
+
     let route = ''
     if(param) {
-      route = `characters?name=${param}&orderBy=name&offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
+      route = `characters?name=${param}&orderBy=name&offset=${offset ? offset : offsetOfCharacters}&limit=${limit || limitOfCharacters}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
     } else {
-      route = `characters?orderBy=name&offset=0&limit=${key.limit}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
+      route = `characters?orderBy=name&offset=${offset ? offset : offsetOfCharacters}&limit=${limit|| limitOfCharacters}&apikey=${key.public_key}&ts=${key.timestamp}&hash=${key.hash}`
     }
 
     try {
       const response = await api.get(route)
-      const { data } = response.data;
-      setCharacters(data.results);
+      const { data } = response.data; 
+      setTotalOfCharacters(data.total)
+      setCharacters([].concat(characters, data.results));
+      setLoading(false);
     } catch (error) {
       console.log(error)
+      setLoading(false);
     }
   }
 
+  const loadMore = () => {
+    if (loading) return;
+
+    let tempLimit = limitOfCharacters;
+    let tempOffset = offsetOfCharacters;
+
+    if (tempLimit === 20) {
+      tempOffset = tempLimit;
+    } else {
+      tempOffset = tempOffset+100;
+    }
+
+    setOffsetOfCharacters(tempOffset)
+    setLimitOfCharacters(100);
+
+    if (tempLimit + 20 < totalOfCharacters) {
+      getCharacters(searchString, 100, tempOffset);
+      getCharacters(searchString, 100, tempOffset);
+    }
+  }
+  
   const like = (id) => {
     let obj = JSON.parse(localStorage.getItem("like")) || {};
 
@@ -55,11 +85,14 @@ function Home() {
   
   const search = (event) => {
     event.persist();
+
+    setSearchString(event.target.value);
+
     let debouncedFn;
     if (!debouncedFn) {
       debouncedFn = debounce(() => {
          let searchString = event.target.value;
-         if (searchString.length > 3) {
+         if (searchString.length >= 3) {
            getCharacters(searchString);
          } 
          if (searchString.length === 0) {
@@ -89,25 +122,27 @@ function Home() {
     <div className="container-characters">
 
       {characters && characters.map( item => (
-        <>
-            <div className="card">
-              <Link key={item.id} to={`/characters/${item.id}`}>
-                <div className="card-poster">
-                  <img 
-                  src={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
-                  alt={item.name}/>
-                </div>
-              </Link>
-            <div className="card-info">
-              <h3>{item.name}</h3>
-                <i className={`fa fa-heart ${JSON.parse(localStorage.getItem("like")) && JSON.parse(localStorage.getItem("like"))[item.id] === true ? 'active' : ''}`} aria-hidden="true" onClick={() => like(item.id)}></i>
+        <div key={item.id} className="card">
+          <Link to={`/characters/${item.id}`}>
+            <div className="card-poster">
+              <img 
+              src={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
+              alt={item.name}/>
             </div>
+          </Link>
+          <div className="card-info">
+            <h3>{item.name}</h3>
+              <i className={`fa fa-heart ${arrayOfLike && arrayOfLike[item.id] === true ? 'active' : ''}`} aria-hidden="true" onClick={() => like(item.id)}></i>
           </div>
-        </>
+        </div>
       ))}
 
 
     </div>
+
+      <div className="btn-primary">
+        <button disabled={totalOfCharacters <= limitOfCharacters && characters.length === totalOfCharacters} onClick={loadMore}>Carregar mais {loading ? <i className="fas fa-spinner fa-spin"></i> : ""}</button>
+      </div>
     </>
   )
 }
